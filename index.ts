2f48d27eff56a0c158924d2f9712bf7fd7ac170d
JelteDirks <j.dirks@outlook.com>
2018-10-15 21:54:15 +0200

    use explicit import/export

--- index.ts
+++ index.ts
@@ -2,4 +2,7 @@ export * from "./src/ts/prettyPrint";
 export * from "./src/ts/Pipe";
 export * from "./src/ts/pad";
 export * from "./src/ts/isType";
-export * from "./src/ts/createPlainElement";
+
+import {Element} from "./src/ts/createPlainElement";
+
+export {Element};
