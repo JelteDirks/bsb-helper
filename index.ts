8f92fd6fd4750e689d09472613658d60e45caf27
JelteDirks <j.dirks@outlook.com>
2018-10-17 21:38:51 +0200

    import and export all src level files

--- index.ts
+++ index.ts
@@ -2,5 +2,28 @@ import {pad} from "./src/pad";
 import {Pipe} from "./src/Pipe";
 import {prettyPrint} from "./src/prettyPrint";
 import {nonEmptyProperty} from "./src/nonEmptyProperty";
+import {listProperties} from "./src/listProperties";
+import {type} from "./src/isType";
+import {isLeapYear} from "./src/isLeapYear";
+import {filterEmptyProperties} from "./src/filterEmptyProperties";
+import {Currency} from "./src/currencyConversion";
+import {cleanEuroFromQuestion, needsEuroCleaning, addEuroToAnswer} from "./src/cleanEuro";
+import {arrayFind} from "./src/arrayFind";
+import {argsToArray} from "./src/argsToArray";
 
-export {pad, Pipe, prettyPrint, nonEmptyProperty};
+export {
+    pad,
+    Pipe,
+    prettyPrint,
+    nonEmptyProperty,
+    listProperties,
+    type,
+    isLeapYear,
+    filterEmptyProperties,
+    Currency,
+    cleanEuroFromQuestion,
+    needsEuroCleaning,
+    addEuroToAnswer,
+    argsToArray,
+    arrayFind
+};
