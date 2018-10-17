745bc2d34b8f0286357090b3c713b30574048db1
JelteDirks <j.dirks@outlook.com>
2018-10-17 09:20:26 +0200

    change main index to exports only

--- index.ts
+++ index.ts
@@ -2,7 +2,4 @@ export * from "./src/ts/prettyPrint";
 export * from "./src/ts/Pipe";
 export * from "./src/ts/pad";
 export * from "./src/ts/isType";
-
-import {Element} from "./src/ts/createPlainElement";
-
-export {Element};
+export * from "./src/ts/createPlainElement/Element";
