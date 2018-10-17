17660ef3d7dc38dd16bc3152f23030897ecc9a75
JelteDirks <j.dirks@outlook.com>
2018-10-17 09:18:15 +0200

    new setup

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export * from "./src/ts/prettyPrint";
+export * from "./src/ts/Pipe";
+export * from "./src/ts/pad";
+export * from "./src/ts/isType";
+import { Element } from "./src/ts/createPlainElement";
+export { Element };
