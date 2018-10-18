1cba639f467f2f8a8769e8f4c55cf23e2d5abf5f
JelteDirks <j.dirks@outlook.com>
2018-10-18 14:50:48 +0200

    add initial value and value setter test

--- src/Pipe.spec.ts
+++ src/Pipe.spec.ts
@@ -1,5 +1,16 @@
 import {Pipe} from "./Pipe";
 
+test('initial value', () => {
+    let p = new Pipe('value');
+    expect(p.getValue()).toBe('value');
+});
+
+test('value setter', () => {
+    let p = new Pipe('');
+    p.setValue(10);
+    expect(p.getValue()).toBe(10);
+});
+
 test('basic chaining', () => {
     function addX(a: number) {
         return a + 10;
