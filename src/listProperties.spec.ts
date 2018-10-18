24b8d3ffa6efbfa313eceb9085ca09ce01237aa0
JelteDirks <j.dirks@outlook.com>
2018-10-18 19:59:49 +0200

    add listProperties tests

---
+++ src/listProperties.spec.ts
@@ -0,0 +1,25 @@
+import {listProperties} from "./listProperties";
+
+test('basic property listing', () => {
+    const testObject = {
+        one: '1',
+        two: '2'
+    };
+
+    const testResult = [
+        {
+            key: 'one',
+            value: '1'
+        },
+        {
+            key: 'two',
+            value: '2'
+        }
+    ];
+
+    expect(listProperties(testObject)).toMatchObject(testResult);
+});
+
+test('empty object literal', () => {
+    expect(listProperties({})).toMatchObject([]);
+});
