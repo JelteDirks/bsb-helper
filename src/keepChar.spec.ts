3cd6a96ec4bd210e9fdd38fc9045269727e4664c
JelteDirks <j.dirks@outlook.com>
2018-10-23 15:32:08 +0200

    add keepChar

---
+++ src/keepChar.spec.ts
@@ -0,0 +1,9 @@
+import {keepChar} from "./keepChar";
+
+test('basic', () => {
+    expect(keepChar('abc0123', 48)).toMatch('0');
+});
+
+test('array', () => {
+    expect(keepChar('abc0123', [48, 49, 50, 51])).toMatch('0123');
+});
---
+++ src/keepChar.ts
@@ -0,0 +1,8 @@
+import {isType} from "./isType";
+
+export function keepChar(s: string, char: number | number[]) {
+    let charCodes: number[] = isType.isArray(char) ? <number[]>char : [<number>char];
+    return Array.prototype.filter.call(s, (c) => {
+        return charCodes.indexOf(c.charCodeAt(0)) > -1;
+    }).join('');
+}
