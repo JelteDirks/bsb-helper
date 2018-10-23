525f98ffc00b9adcfd95448092db36ed23af29ea
JelteDirks <j.dirks@outlook.com>
2018-10-23 15:20:18 +0200

    currency conversion update

---
+++ src/currencyConversion.spec.ts
@@ -0,0 +1,8 @@
+import {Currency} from "./currencyConversion";
+
+
+
+
+test('basic', () => {
+    let x = new Currency(',', '10.000,00');
+});
--- src/currencyConversion.ts
+++ src/currencyConversion.ts
@@ -1,9 +1,22 @@
+import {isType} from "./isType";
+import {numberToStringCurrency} from "./numberToStringCurrency";
+
 export class Currency {
 
-    constructor(private initialValue: string | number) {
+    private initialValue: string | number;
+    private thousandSeparator: string;
+    private value: string;
+
+    constructor(private decimalSeparator: string, initialValue?: string | number) {
+        this.initialValue = initialValue;
+
+        if (isType.isNumber(initialValue)) {
+            this.value = numberToStringCurrency(<number>initialValue, decimalSeparator);
+        }
     }
 
     public getInitialValue(): string {
         return String(this.initialValue);
     }
 }
+
