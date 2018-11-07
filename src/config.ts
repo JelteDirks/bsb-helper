d6745b1edaa3b99827c077046b7e30a408464e25
JelteDirks <j.dirks@outlook.com>
2018-11-07 13:59:06 +0100

    add config file with constants

---
+++ src/config.ts
@@ -0,0 +1,41 @@
+const forms: FormInfo[] = [
+    {
+        number: 'X0100',
+        subject: 'Nieuwe polis'
+    },
+    {
+        number: 'X0101',
+        subject: 'Gewijzigde polis'
+    },
+    {
+        number: 'X0102',
+        subject: 'Opschorting verzekering'
+    },
+    {
+        number: 'X0103',
+        subject: 'Gewijzigde polis'
+    },
+    {
+        number: 'X0104',
+        subject: 'Gewijzigde polis'
+    },
+    {
+        number: 'X0105',
+        subject: 'Gewijzigde polis'
+    },
+    {
+        number: 'X0106',
+        subject: 'Gewijzigde polis'
+    },
+    {
+        number: 'X0107',
+        subject: 'Gewijzigde polis'
+    }
+];
+
+interface FormInfo {
+    number: string;
+    subject: string;
+}
+
+export {forms, FormInfo};
