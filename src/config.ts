9ed3c2a23c17408fffa30a452a9f7c9ccbd8dd61
JelteDirks <j.dirks@outlook.com>
2018-11-09 11:44:01 +0100

    add forms to config

--- src/config.ts
+++ src/config.ts
@@ -13,23 +13,71 @@ const FORMS: FormInfo[] = [
     },
     {
         number: 'X0103',
-        subject: 'Gewijzigde polis'
+        subject: 'Royement n.a.v. overlijden'
     },
     {
         number: 'X0104',
-        subject: 'Gewijzigde polis'
+        subject: 'Beëindiging verzekering'
     },
     {
         number: 'X0105',
-        subject: 'Gewijzigde polis'
+        subject: 'Royement i.v.m. wanbetaling'
     },
     {
         number: 'X0106',
-        subject: 'Gewijzigde polis'
+        subject: 'Overvoer'
     },
     {
         number: 'X0107',
-        subject: 'Gewijzigde polis'
+        subject: 'Uw gewijzigde polis'
+    },
+    {
+        number: 'X0108',
+        subject: 'Nieuwe verzekeraar'
+    },
+    {
+        number: 'X0109',
+        subject: 'Omzetting TopGarant pakket'
+    },
+    {
+        number: 'X0110',
+        subject: 'Omzetting Segway polis'
+    },
+    {
+        number: 'X0111',
+        subject: 'Beëindiging polis'
+    },
+    {
+        number: 'X0112',
+        subject: 'Nieuwe pakketpolis'
+    },
+    {
+        number: 'X0113',
+        subject: 'Uw verzekeringspakket bij Herland'
+    },
+    {
+        number: 'X0114',
+        subject: 'Garantie tegen onderverzekering'
+    },
+    {
+        number: 'X0115',
+        subject: 'Uw gewijzigigde polis naar aanleiding van het opgaveformulier bedrijfsgegevens'
+    },
+    {
+        number: 'X0116',
+        subject: 'Wijziging inboedelverzekering'
+    },
+    {
+        number: 'X0117',
+        subject: 'Digitale berekening verzekerde som'
+    },
+    {
+        number: 'X0118',
+        subject: 'Pakketomzetting LONDON'
+    },
+    {
+        number: '',
+        subject: ''
     }
 ];
 
