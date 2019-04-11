0326797b4595187d1d8718f852ddd1ded90742c1
Dahfrenk <dah@live.nl>
2019-04-11 09:44:26 +0200

    update questiontype check

--- src/questionTypeCheck.ts
+++ src/questionTypeCheck.ts
@@ -1,10 +1,23 @@
-export function questionTypeCheck(name: string): string {
+export function questionTypeCheck(name: string, value: string): string|undefined {
 
+    if (value == '') {
+        return;
+    }
     if (/^euro__/.test(name)) {
-        return "euro";
+
+        return 'euro';
     } else if (/^x__/.test(name)) {
-        return "percentage";
+        let original_name = name.replace(/^x__/, '');
+        return 'percent';
+    } else if (/^jaar__/.test(name)) {
+        let original_name = name.replace(/^jaar__/, '');
+        return 'jaar';
+    } else if (/^km__/.test(name)) {
+        let original_name = name.replace(/^km__/, '');
+        return 'km';
+    } else if (/^mainswitch_/.test(name) || /^status_/.test(name)) {
+        return;
     } else {
-        return "default";
+        return 'default';
     }
 }
