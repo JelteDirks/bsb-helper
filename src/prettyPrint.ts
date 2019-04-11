3d6846a7d3a4f23c72401b99aabe4d374b9a1e28
Dahfrenk <dah@live.nl>
2019-04-11 11:02:35 +0200

    update prettyprint to filter and replace every special character.

--- src/prettyPrint.ts
+++ src/prettyPrint.ts
@@ -1,9 +1,11 @@
 export function prettyPrint(value: string): string {
-    return Array.prototype.map.call(value, function (character: string, index: number) {
-        if (index === 0) return character.toUpperCase();
-        if (character === "__procent__") return "%";
-        if (character === "__vraag__") return "?";
-        if (character === "_") return " ";
-        return character;
-    }).join("");
+
+    let regex = /^(x|jaar|km)__/g;
+    value = value.replace(regex, '');
+    value = value.replace(/__procent__/g, '%');
+    value = value.replace(/__vraag__/g, '?');
+    value = value.replace(/_/g, ' ');
+    value = value.substring(0,1).toUpperCase()+value.substring(1, value.length);
+
+    return value;
 }
