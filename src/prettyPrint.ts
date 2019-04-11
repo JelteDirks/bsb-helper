396dc7bd5936df0091564faf1174015311a69c7e
Dahfrenk <dah@live.nl>
2019-04-11 08:59:32 +0200

    add inline special characters

--- src/prettyPrint.ts
+++ src/prettyPrint.ts
@@ -1,6 +1,8 @@
 export function prettyPrint(value: string): string {
     return Array.prototype.map.call(value, function (character: string, index: number) {
         if (index === 0) return character.toUpperCase();
+        if (character === "__procent__") return "%";
+        if (character === "__vraag__") return "?";
         if (character === "_") return " ";
         return character;
     }).join("");
