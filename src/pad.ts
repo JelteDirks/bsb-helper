4785734c76b8c8bcf12dc5265793c9be945a901e
Jelte Dirks <j.dirks@outlook.com>
2019-04-28 17:32:21 +0200

    add docs to the pad function
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/pad.ts
+++ src/pad.ts
@@ -1,3 +1,10 @@
+/**
+ * Pads a string with another string until it has reached a certain length.
+ * @param {string} value Value that should be padded with the character until it has the length
+ * @param {string} character Character that should be added to the value
+ * @param {number} length The length at which the padding should stop.
+ * @return {string}
+ */
 export function pad(value: string, character: string, length: number): string {
     for (let i = 0; (value.length + character.length) < (length + 1); i++) {
         value = character + value;
