a21de32cc9701c8e25ea729e8fe99ba0430b75e9
Jelte Dirks <j.dirks@outlook.com>
2019-10-02 18:28:01 +0200

    update readability for valid currency
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/isValidCurrency.ts
+++ src/isValidCurrency.ts
@@ -1,4 +1,6 @@
 export function isValidCurrency(val: string, decSep: string) {
-    return (decSep === "," ? new RegExp(`(?=.*\\d)^-?[0-9]\\d{0,2}(\\.\\d{3})*(,\\d{2})?$`) :
-        new RegExp(`(?=.*\\d)^-?[0-9]\\d{0,2}(,\\d{3})*(\\.\\d{2})?$`)).test(val);
+    const eu = new RegExp(`(?=.*\\d)^-?[0-9]\\d{0,2}(\\.\\d{3})*(,\\d{2})?$`);
+    const us = new RegExp(`(?=.*\\d)^-?[0-9]\\d{0,2}(,\\d{3})*(\\.\\d{2})?$`);
+
+    return (decSep === "," ? eu : us).test(val);
 }
