44fdfeae5d30e4cb832bb29c16e5b105c48c0fdb
Jelte Dirks <j.dirks@outlook.com>
2019-10-14 10:45:35 +0200

    add test for entire sentence
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/strToHex.spec.ts
+++ tests/strToHex.spec.ts
@@ -2,12 +2,18 @@ import {strToHex} from "../src/strToHex";
 
 describe('string to hex conversion', () => {
 
-    test('should covert simple word', () => {
+    test('should convert simple word', () => {
         expect(strToHex('jelte')).toMatch('6a656c7465')
     });
 
-    test('should covert symbols', () => {
-        expect(strToHex(':,./;\'[]-=<>?:"{}_+!@@#$%^&*()`~')).toMatch('2c2e2f3b275b5d2d3d3c3e3f3a227b7d5f2b2140402324255e262a2829607e')
+    test('should convert symbols', () => {
+        expect(strToHex(':,./;\'[]-=<>?:"{}_+!@@#$%^&*()`~'))
+            .toMatch('2c2e2f3b275b5d2d3d3c3e3f3a227b7d5f2b2140402324255e262a2829607e')
+    });
+
+    test('should convert sentences', () => {
+        expect(strToHex('Dit is een zin met woorden, leestekens en de getallen 1, 2, 3 en 0.'))
+            .toMatch('4469742069732065656e207a696e206d657420776f6f7264656e2c206c65657374656b656e7320656e20646520676574616c6c656e20312c20322c203320656e20302e')
     });
 
 });
