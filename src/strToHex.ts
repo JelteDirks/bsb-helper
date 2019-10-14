9868153234414ac8a1f8da4741d26f25a61c8d4f
Jelte Dirks <j.dirks@outlook.com>
2019-10-14 10:52:12 +0200

    implement function strToHex
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/strToHex.ts
+++ src/strToHex.ts
@@ -1,3 +1,5 @@
 export function strToHex(value: string): string {
-    return value;
+    return Array.prototype.map.call(value, (c: string) => {
+        return c.charCodeAt(0).toString(16);
+    }).join('');
 }
