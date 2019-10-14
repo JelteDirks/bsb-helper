ec0d09078d470ad179c57929f23d8b59669e1f4a
Jelte Dirks <j.dirks@outlook.com>
2019-10-14 10:43:57 +0200

    add test for converting symbols
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/strToHex.spec.ts
+++ tests/strToHex.spec.ts
@@ -6,4 +6,8 @@ describe('string to hex conversion', () => {
         expect(strToHex('jelte')).toMatch('6a656c7465')
     });
 
+    test('should covert symbols', () => {
+        expect(strToHex(':,./;\'[]-=<>?:"{}_+!@@#$%^&*()`~')).toMatch('2c2e2f3b275b5d2d3d3c3e3f3a227b7d5f2b2140402324255e262a2829607e')
+    });
+
 });
